566b37cce9307726ed6ae868addddbeb71ad1b3b
eaton <ew9170@rit.edu>
2024-05-21 22:54:25 -0400

    Correct implementation of the perceptron algorithm

--- feed-forward/src/lib.rs
+++ feed-forward/src/lib.rs
@@ -26,9 +26,9 @@ pub mod perceptron {
         }
 
         /// Given a set of data, normalizes the data to be between 0.0 and 1.0
-        pub fn normalize(data: ndarray::Array2<f64>) -> ndarray::Array2<f64>{
-            let mut highest = 0f64;
-            let mut lowest = f64::INFINITY;
+        pub fn normalize(data: &ndarray::Array2<u8>) -> ndarray::Array2<f64>{
+            let mut highest = u8::MIN;
+            let mut lowest = u8::MAX;
             let mut return_data = data.clone();
             for i in data.iter() {
                 if *i > highest {
@@ -41,7 +41,8 @@ pub mod perceptron {
             for i in return_data.iter_mut() {
                 *i = (*i - lowest) / (highest - lowest);
             }
-            return return_data;
+            // convert to f64
+            return_data.mapv(|x| x as f64)
         }
 
         /// Performs the "Perceptron Algorithm" given some set of data "training_data"
@@ -55,32 +56,51 @@ pub mod perceptron {
         ///         ii.) otherwise, we update weights by multiplying the feature by a - predict(x).
         ///
         /// The samples are organized as rows, with the last column being the ground truth.
-        pub fn train(&mut self, training_data: ndarray::Array2<f64>, n_iterations: usize) {
+        pub fn train(&mut self, training_data: &ndarray::Array2<u8>, training_labels: &Vec<u8>, n_iterations: usize) {
             // we find the highest and lowest values, and normalize:
             let normalized_data = Self::normalize(training_data);
 
             // we iterate over the training data N times, or until the weights don't change
-            for _ in 0..n_iterations {
+            for i in 0..n_iterations {
                 let prev_weights = self.weights.clone();
-                // for each row/sample in the normalized data, we separate out the ground truth
-                for row in normalized_data.rows() {
+                for (idx, row) in normalized_data.outer_iter().enumerate() {
                     // cloning a slice turns the view into an owned array
-                    let x = row.slice(s![0..-1]).clone();
-                    let a = row[row.len() - 1];
+                    // println!("Row {}: {:?}", idx, row.clone());
+                    let x = row.clone(); // the sample
+                    let a = training_labels[idx] as f64; // the ground truth
                     let prediction = self.predict(x);
                     if a - prediction == 0f64 { // if the prediction is correct, we continue
+                        // println!("Prediction {} was correct, continuing", prediction);
                         continue;
                     }
                     // prediction was incorrect, update weights
-                    for (i, weight) in self.weights.iter_mut().enumerate() {
-                        *weight += (a - prediction) * x[i];
+                    if prediction == 0f64 && a == 1f64 { // false negative
+                        self.weights += &x;
+                    } else if prediction == 1f64 && a == 0f64 { // false positive
+                        self.weights -= &x;
                     }
                 }
                 // if the weights haven't changed, we break out of the loop
                 if prev_weights == self.weights {
+                    println!("Converged at n = {}, breaking loop", i);
                     break;
                 }
             }
         }
+
+        /// Given some set of validation images, and their labels,
+        /// and print out the predictions of the model, as well as the accuracy.
+        pub fn validate(&self, validation_images: &ndarray::Array2<u8>, validation_labels: &Vec<u8>) {
+            let normalized_validation_images = Self::normalize(validation_images);
+            let mut correct_predictions = 0;
+            for (idx, row) in normalized_validation_images.outer_iter().enumerate() {
+                let prediction = self.predict(row);
+                if prediction == validation_labels[idx] as f64 {
+                    correct_predictions += 1;
+                }
+            }
+            println!("Accuracy: {}", correct_predictions as f64 / validation_labels.len() as f64);
+        }
+
     }
 }
